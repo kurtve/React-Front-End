22e7178eb3fe7933739c333e93d522e59b5992d4
Kurt Van Etten <kurt@vanetten.us>
2019-09-21 22:15:44 -0400

    Synchronize with Clif branch

--- react-front-end/src/Components/Nav/TopNav.js
+++ react-front-end/src/Components/Nav/TopNav.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-export default function Nav() {
+export default function Nav({logo}) {
+
+  const StyledTopNav = styled.div `
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 480px;
+    padding: 2% 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    background: #00a0ba;
+  `
+
   return (
-    <div>
-      Nav
-    </div>
+    <StyledTopNav>
+      <Link to="/">
+        <img src={logo} alt="Design Your Life Logo" style={{ height: '50px', width: 'auto', fill: '#00a0ba'}}/>
+      </Link>
+    </StyledTopNav>
   )
 }
