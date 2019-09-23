e8f696cabf50befbdd5969d73a4ba467d693b67b
Kurt Van Etten <kurt@vanetten.us>
2019-09-23 00:29:30 -0400

    Finish ConfirmDelete functionality plus add time (duration) to ActivityCard

--- react-front-end/src/Components/Activities/ConfirmDelete.js
+++ react-front-end/src/Components/Activities/ConfirmDelete.js
@@ -25,19 +25,19 @@ const StyledConfirmDelete = styled.div `
 	}
 
 	button {
-		font-size: 1.4rem;
+		font-size: 1.8rem;
 		color: white;
 		border-radius: 10px;
 		border: none;
-		width: 70px;
-		height: 25px;
+		width: 90px;
+		height: 35px;
 
 		&:hover {
 			cursor: pointer;
 		}
 	}
 
-	button.edit {
+	button.cancel {
 		background-color: #00bc98;
 	}
 
@@ -50,7 +50,7 @@ const StyledConfirmDelete = styled.div `
 		flex-direction: row;
 		justify-content:  space-between;
 		align-items: center;
-		padding: 5px;
+		padding: 30px 10px 10px;
 		width: 90%;
 	}
 
@@ -67,6 +67,10 @@ const StyledConfirmDelete = styled.div `
 		font-size 1.6rem;
 	}
 
+	.time {
+		font-size: 1.6rem;
+	}
+	
 	.notes {
 		font-size: 1.6rem;
 	}
@@ -81,34 +85,49 @@ const StyledConfirmDelete = styled.div `
 
 export default function ConfirmDelete(props) {
 
+	// get the id value from the path
+	const id = props.match.params.id;
+
+	// find the activity in the activities array
+	const index = props.activities.findIndex(item => item.id === id);
+	if (index < 0) {
+		// something went wrong. return to activities feed.
+		props.history.push('/activities');
+	}
+
+	// get the activity
+	const activity = props.activities[index];
+
+	// confirmed! delete the activity and return to activities feed
 	const deleteActivity = (id) => {
-		console.log(`delete activity ${id}`);
+		props.deleteActivity(id);
 		props.history.push('/activities');
 	};
 
+	// user changed their mind. return to activities feed
 	const cancel = () => {
 		props.history.push('/activities');
 	};
 
 	return (
     	<StyledConfirmDelete>
+    		<h1>Delete this activity?</h1>
+
+	    	<div className='name'>{activity.name}</div>
+    		<div className='category'>Category: {activity.category}</div>
+    		<div className='rating'>Rating: {activity.rating}</div>
+    		<div className='time'>Duration: {activity.time} minutes</div>
+    		<div className='notes'>{activity.notes}</div>
+    		<div className='timestamps'>Created: {activity.created.substring(0, 16)}
+    			{activity.updated && `Updated: ${activity.updated.substring(0, 16)}`}</div>
+
     		<div className='footer'>
     			<button onClick={cancel}
     			 className='cancel'>Cancel</button>
-	    		<button onClick={() => deleteActivity(1)}
-	    		 className='delete'>Confirm Delete</button>
+	    		<button onClick={() => deleteActivity(activity.id)}
+	    		 className='delete'>Delete</button>
     		</div>
 		</StyledConfirmDelete>
 	);
 
 }
-
-
-/*
-	    	<div className='name'>{props.activity.name}</div>
-    		<div className='category'>Category: {props.activity.category}</div>
-    		<div className='rating'>Rating: {props.activity.rating}</div>
-    		<div className='notes'>{props.activity.notes}</div>
-    		<div className='timestamps'>Created: {props.activity.created.substring(0, 16)}
-    			{props.activity.updated && `Updated: ${props.activity.updated.substring(0, 16)}`}</div>
-*/
