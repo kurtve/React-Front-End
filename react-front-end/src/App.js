6da435b99aae0365476693c41e0130cc3e291eb5
Andrew Nottoli <nottolivc@gmail.com>
2019-09-23 23:23:18 -0700

    created basic functional interface for input, needs styling, needs api as still renders locally, needs updates to match other branches

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,11 +1,96 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Nav from './Components/Nav/Nav';
+
+import Welcome from './Components/Welcome/Welcome';
+import ActivitiesFeed from './Components/ActivitiesFeed/ActivitiesFeed.js';
+import ConfirmDelete from './Components/ActivitiesFeed/ConfirmDelete.js';
+import Insights from './Components/Insights/Insights.js';
+import AddActivityForm from './Components/ActivitiesFeed/AddActivityForm.js';
+import EditActivityForm from './Components/ActivitiesFeed/EditActivityForm.js';
+
+/* functions for maintaining state, etc */
+import * as helpers from './Components/Functions/HelperFunctions.js'; 
+
 
 function App() {
+
+  const StyledApp = styled.div `
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+  `;
+
+
+  const [activities, setActivities] = useState(helpers.initialActivities);
+  const [insights, setInsights] = useState(helpers.initialInsights);
+
+
+  // helpers.add adds an item to an array in state
+  // it will add an id and timestamp to the object before adding it to the list
+  const addActivity = (newActivity) => {
+    helpers.add(newActivity, activities, setActivities);
+  };
+
+  const addInsight = (newInsight) => {
+    helpers.add(newInsight, insights, setInsights);
+  };
+
+
+  // helpers.delete removes an item to an array in state
+  // if the id is not found in the array, no change takes place
+  const deleteActivity = (activityId) => {
+    helpers.remove(activityId, activities, setActivities);
+  };
+
+  const deleteInsight = (insightId) => {
+    helpers.remove(insightId, insights, setInsights);
+  };
+
+
+  // helpers.edit replaces an item in an array in state with an updated version
+  // if the id of the item is not found in the array, no change takes place
+  const editActivity = (activity) => {
+    helpers.edit(activity, activities, setActivities);
+  };
+
+  const editInsight = (insight) => {
+    helpers.edit(insight, insights, setInsights);
+  };
+
+
   return (
-    <>
-     Design Your Life App 
-    </>
+    <StyledApp>
+
+      <Nav />
+
+      <Route exact path="/" component={Welcome} />
+
+      <Route path='/activities' render={(props) => (
+        <ActivitiesFeed {...props} activities={activities} />
+      )} />
+
+      <Route path='/deleteactivity/:id' render={(props) => (
+        <ConfirmDelete {...props} activities={activities} deleteActivity={deleteActivity} />
+      )} />
+
+      <Route path='/insights' render={(props) => (
+        <Insights {...props} insights={insights} activities={activities}
+         addInsight={addInsight} editInsight={editInsight} deleteInsight={deleteInsight} />
+      )} />
+
+      <Route path='/addactivity' render={(props) => (
+        <AddActivityForm {...props} addActivity={addActivity} />
+      )} />
+
+      <Route path='/editactivity/:id' render={(props) => (
+        <EditActivityForm {...props} editActivity={editActivity} />
+      )} />
+
+    </StyledApp>
   );
 }
 
