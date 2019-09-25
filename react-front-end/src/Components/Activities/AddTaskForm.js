85a18515bcc3a4618a02cc093516f3f24897ca86
Andrew Nottoli <nottolivc@gmail.com>
2019-09-24 21:55:15 -0700

    added post to API axios, styling comp still, API rejecting Data still

---
+++ react-front-end/src/Components/Activities/AddTaskForm.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const AddTaskForm = props => {
+    const initialFormState = {userId: undefined, activityName:'', category:'', duration:'', description:'', createdDate:'', energyLevel:'', enjoymentLevel:''}
+    const [ task, setTask ] = useState(initialFormState)
+  
+    const handleInputChange = event => {
+      const { name, value } = event.target
+  
+      setTask({ ...task, [name]: value })
+    }
+  
+    return (
+      <div className="AddTaskForm">
+      <form
+        onSubmit={event => {
+          event.preventDefault()
+          if (!task.userId || !task.activity) return
+  
+          props.addTask(task)
+          axios.post('https://design-your-life-backend.herokuapp.com/api/activity',task) 
+          .then(data => {
+            console.log(data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+          setTask(initialFormState); 
+        }
+      }
+      >
+        <label>Activities</label>
+        <br />
+
+        <br />
+        <label>User Id: </label>
+        <input type="text" name="userId" placeholder="user" value={task.userId} onChange={handleInputChange} />
+        <br />
+        <label>Activity</label>
+        <input type="text" name="activity" placeholder="activity" value={task.ActivityName} onChange={handleInputChange} />
+        <br />
+        <label>Category </label>
+        <input type="text" name="category" placeholder="Category" value={task.category} onChange={handleInputChange} />
+        <br />
+        <label>Duration </label>
+        <input type="number" name="duration" placeholder="time (minutes)" value={task.duration} onChange={handleInputChange} />
+        <br />
+        <label>Descript. </label>
+        <input type="text" name="description" placeholder="Add Description..." value={task.description} onChange={handleInputChange} /> 
+        <br />
+        <label>Date </label>
+        <input type="text" name="createdDate" placeholder="createdDate" value={task.createdDate} onChange={handleInputChange} />
+        <br />
+        <label>Energy </label>
+        <input type="text" name="energyLevel" placeholder="energy-level" value={task.energyLevel} onChange={handleInputChange} />
+        <br />
+        <label>Enjoyment</label>
+        <input type="text" name="enjoymentLevel" placeholder="rating" value={task.enjoymentLevel} onChange={handleInputChange} />
+        <br />
+        <button>Add Task</button>
+        </form>
+        </div>
+    )
+  }
+  
+  export default AddTaskForm;
