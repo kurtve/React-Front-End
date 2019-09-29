e2a97239c2664d40b7e1d6fc860f07c5487612e6
Kurt Van Etten <kurt@vanetten.us>
2019-09-29 18:04:04 -0400

    removed console.log statements that are no longer needed

--- react-front-end/src/Components/Activities/ConfirmDelete.js
+++ react-front-end/src/Components/Activities/ConfirmDelete.js
@@ -89,9 +89,6 @@ export default function ConfirmDelete(props) {
 	// get the id value from the path
 	const id = Number.parseInt(props.match.params.id);
 
-	console.log('in confirm delete');
-	console.log(props.activities);
-
 	// find the activity in the activities array
 	const index = props.activities.findIndex(item => item.id === id);
 	if (index < 0) {
